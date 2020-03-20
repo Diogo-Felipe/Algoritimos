ba6589716f74d03513756b026bef56c06e419593
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-20 12:08:57 -0300

    Adição do main

---
+++ Estruturas de Dados/javascript/main.js
@@ -0,0 +1,8 @@
+let listaSimplesmenteEncadeada = new ListaSimplesmenteEncadeada();
+
+listaSimplesmenteEncadeada.addInicio(1);
+listaSimplesmenteEncadeada.addFim(2);
+listaSimplesmenteEncadeada.addInicio(3);
+listaSimplesmenteEncadeada.ler();
+listaSimplesmenteEncadeada.remove(2);
+listaSimplesmenteEncadeada.ler();
